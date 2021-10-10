3bca4f4879099de265bc682aca7316f2c85d8a95
Daniel Uboa <uboadaniel@gmail.com>
2021-10-10 17:57:06 +0300

    New changes

--- apps/carts/cart.py
+++ apps/carts/cart.py
@@ -1,14 +1,33 @@
 from django.conf import settings
 
+from apps.store.models import Product
+
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get(settings.SESSION_COOKIE_AGE)
+        cart = self.session.get(settings.CART_SESSION_ID)
 
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        product_clean_ids = []
+
+        for p in product_ids:
+            product_clean_ids.append(p)
+
+            self.cart[str(p)]['product'] = Product.objects.get(pk=p)
+
+        for item in self.cart.values():
+            item['total_price'] = int(item['price']) * int(item['quantity'])
+
+            yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
